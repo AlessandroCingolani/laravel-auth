eb9125419787e974d1b2b207906564b87c0a8a7b
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2023-11-29 14:40:00 +0100

    [ add ] btn return prev page

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -5,4 +5,5 @@
     <p><strong>Start date:</strong> {{ $project->start_date }}</p>
     <p><strong>End date:</strong> {{ $project->end_date }}</p>
     <p><strong>Description:</strong> {{ $project->description }}</p>
+    <a href="javascript:history.back()" class="btn btn-primary">Return</a>
 @endsection
