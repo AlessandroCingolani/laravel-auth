d0afed82f9fdbfd0b499713fd6f7c0a654e7dc20
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2023-11-30 17:09:28 +0100

    [ change ] naming fillable

--- app/Http/Requests/ProjectRequest.php
+++ app/Http/Requests/ProjectRequest.php
@@ -23,7 +23,7 @@ class ProjectRequest extends FormRequest
     {
         return [
             'name' => 'required|min:2|max:100',
-            'text' => 'required|min:5',
+            'description' => 'required|min:5',
             'link' => 'required|min:5'
         ];
     }
