c47b49088fe660a1e0814c417ba5d858761acea7
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2023-11-30 17:33:42 +0100

    [ add ] helper and session success

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -1,7 +1,16 @@
+@php
+    use App\Functions\Helper;
+@endphp
+
 @extends('layouts.admin')
 
 @section('content')
     <h1>List projects</h1>
+    @if (session('success'))
+        <div class="alert alert-success" role="alert">
+            {{ session('success') }}
+        </div>
+    @endif
     <table class="table table-bordered">
         <thead>
             <tr>
@@ -17,11 +26,18 @@
                 <tr>
                     <td>{{ $project->id }}</td>
                     <td>{{ $project->name }}</td>
-                    <td>{{ $project->start_date }}</td>
+                    <td>{{ Helper::formatDate($project->start_date) }}</td>
                     <td>{{ isset($project->end_date) ? "$project->end_date" : 'Work in progress' }}</td>
                     <td class="text-center">
                         <a href="{{ route('admin.projects.show', $project) }}" class="btn btn-success"> <i
                                 class="fa-solid fa-circle-info"></i></a>
+                        <a href="{{ route('admin.projects.edit', $project) }}" class="btn btn-warning"><i
+                                class="fa-solid fa-pencil"></i></a>
+
+                        @include('admin.partials.formDelete', [
+                            'route' => route('admin.projects.destroy', $project),
+                            'message' => 'Are you sure you want to delete this project?',
+                        ])
                     </td>
 
                 </tr>
