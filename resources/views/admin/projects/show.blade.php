de571926dc9e89bd589cbec7ae44bf5fae92e145
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2023-11-30 17:10:46 +0100

    [ add ] ternary for end_date

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <h3>{{ $project->name }}</h3>
     <p><strong>Start date:</strong> {{ $project->start_date }}</p>
-    <p><strong>End date:</strong> {{ $project->end_date }}</p>
+    <p><strong>End date:</strong> {{ isset($project->end_date) ? "$project->end_date" : 'Work in progress' }}</p>
     <p><strong>Description:</strong> {{ $project->description }}</p>
-    <a href="javascript:history.back()" class="btn btn-primary">Return</a>
+    <a href="{{ route('admin.projects.index') }}" class="btn btn-primary">Return</a>
 @endsection
